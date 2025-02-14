1a1a456b885f732f8f853b780f8aef8928f99356
krotla <kkornel@o2.pl>
2025-02-14 20:53:59 +0100

    Sort letters count in the report by count descending

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def main():
     text = load_text(file_path)
     words_number = count_words(text)
     char_counts = count_chars(text)
-    print_report(file_path, words_number, char_counts)
+    char_counts_sorted = dict_to_sorted_list(char_counts)
+    print_report(file_path, words_number, char_counts_sorted)
     
 def load_text(file):
     with open(file) as f:
@@ -26,12 +27,22 @@ def count_chars(text):
             char_counts[character] = 1
     return char_counts
 
+def dict_to_sorted_list(dictionary):
+    new_list = []
+    for key in dictionary:
+        new_list.append({"char": key, "count": dictionary[key]})
+    new_list.sort(key=sort_on, reverse=True)
+    return new_list
+
+def sort_on(dictionary):
+    return dictionary["count"]
+
 def print_report(file_path, words_number, char_counts):
     print(f"--- Begin report of {file_path} ---")
     print(f"{words_number} words found in the document\n")
-    for c in char_counts:
-        if c.isalpha():
-            print(f"The '{c}' character was found {char_counts[c]} times")
+    for char_info in char_counts:
+        if char_info["char"].isalpha():
+            print(f"The '{char_info["char"]}' character was found {char_info["count"]} times")
     print("--- End report ---")
 
 
